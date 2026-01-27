4ba62fdb38be1e8eb3f95c1863792f5f68d21e57
Nikodem Okroj <n.okroj@icloud.com>
2026-01-27 18:09:31 +0100

    fix(apps/api): parse port environment variable as integer

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(process.env.PORT ?? 8000);
+  await app.listen(process.env.PORT ? parseInt(process.env.PORT, 10) : 8000);
 }
 bootstrap();
