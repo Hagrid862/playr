5fb19c504af8a5bed44c128b78d21c8974241dc8
Nikodem Okroj <n.okroj@icloud.com>
2026-01-27 22:22:30 +0100

    feat(apps/api): added configModule to main appModule

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [SharedModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), SharedModule],
   controllers: [AppController],
   providers: [AppService],
   exports: [],
