4f3a98282631848535ed84149beb682ee6b27bd1
Nikodem Okroj <n.okroj@icloud.com>
2026-01-28 11:00:04 +0100

    tests(apps/api): Replaced jest with faster vitest - also renamed e2e tests inside api to integration tests since we will use aditionally playwright for deep and real e2e tests

--- apps/api/integration/mocks/prisma.service.mock.ts
+++ apps/api/integration/mocks/prisma.service.mock.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@repo/db';
-import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
+import { DeepMockProxy, mockDeep } from 'vitest-mock-extended';
 
 @Injectable()
 export class PrismaServiceMock {
