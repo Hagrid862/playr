175eb912c33abea7abdf625213624c789225f2a8
Nikodem Okroj <n.okroj@icloud.com>
2026-01-31 14:58:36 +0100

    feat(apps/api): Applied global response interceptor and exception filter in api main pipeline

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,8 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +15,9 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
+
+  app.useGlobalFilters(new GlobalExceptionFilter());
+  app.useGlobalInterceptors(new ResponseInterceptor(app.get(Reflector)));
 
   await app.listen(process.env.PORT ? parseInt(process.env.PORT, 10) : 8000);
 }
