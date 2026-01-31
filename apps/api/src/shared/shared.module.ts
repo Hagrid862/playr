a165964053dea355cb71d55054e2ae434d4b2b12
Nikodem Okroj <n.okroj@icloud.com>
2026-01-31 22:48:40 +0100

    chore(apps/api): Added users repository to shared module

--- apps/api/src/shared/shared.module.ts
+++ apps/api/src/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './services/prisma/prisma.service';
+import { UserRepository } from './repositories/user.repository';
 
 @Global()
 @Module({
   imports: [],
   controllers: [],
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [PrismaService, UserRepository],
+  exports: [PrismaService, UserRepository],
 })
 export class SharedModule {}
