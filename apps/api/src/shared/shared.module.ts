7f3c6e15aaf82262206ce544c4d1734667624d98
Nikodem Okroj <n.okroj@icloud.com>
2026-02-01 15:27:52 +0100

    chore(apps/api): Added email repository to shared module

--- apps/api/src/shared/shared.module.ts
+++ apps/api/src/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './services/prisma/prisma.service';
 import { UserRepository } from './repositories/user.repository';
+import { EmailAddressRepository } from './repositories/email-address.repository';
 
 @Global()
 @Module({
   imports: [],
   controllers: [],
-  providers: [PrismaService, UserRepository],
-  exports: [PrismaService, UserRepository],
+  providers: [PrismaService, UserRepository, EmailAddressRepository],
+  exports: [PrismaService, UserRepository, EmailAddressRepository],
 })
 export class SharedModule {}
