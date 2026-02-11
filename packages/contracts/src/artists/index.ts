35bc9300edc9c3bc467b2a8d6596da952c3f856b
Nikodem Okroj <n.okroj@icloud.com>
2026-02-11 20:33:46 +0100

    feat(packages/contracts): add artist banner upload contract
    Add request and response schemas for uploading artist banner images,
    following the same pattern as the existing avatar upload functionality.

--- packages/contracts/src/artists/index.ts
+++ packages/contracts/src/artists/index.ts
@@ -3,4 +3,5 @@ export * from "./delete-artist";
 export * from "./get-private-artists";
 export * from "./update-artist";
 export * from "./upload-artist-avatar";
+export * from "./upload-artist-banner";
 
