7d75c6705fee530c9f982c700dddd80823057722
Nikodem Okroj <n.okroj@icloud.com>
2026-02-12 20:45:39 +0100

    refactor(apps/api): remove private profile feature module

--- apps/api/src/features/features.module.ts
+++ apps/api/src/features/features.module.ts
@@ -3,10 +3,9 @@ import { AlbumsModule } from './albums/albums.module';
 import { ArtistsModule } from './artists/artists.module';
 import { AuthModule } from './auth/auth.module';
 import { LibraryModule } from './library/library.module';
-import { PrivateProfileModule } from './private-profile/private-profile.module';
 
 @Module({
-  imports: [AuthModule, LibraryModule, PrivateProfileModule, ArtistsModule, AlbumsModule],
+  imports: [AuthModule, LibraryModule, ArtistsModule, AlbumsModule],
   controllers: [],
   providers: [],
   exports: [],
--- apps/api/src/features/private-profile/dto/create-private-profile.response.dto.ts
+++
@@ -1,6 +0,0 @@
-import { CreatePrivateProfileResponseSchema } from '@repo/contracts';
-import { createZodDto } from 'nestjs-zod';
-
-export class CreatePrivateProfileResponseDto extends createZodDto(
-  CreatePrivateProfileResponseSchema,
-) {}
