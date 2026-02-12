d43cd1251ae96e9de8a3bf659acfd6da82f1ee69
Nikodem Okroj <n.okroj@icloud.com>
2026-02-12 16:22:31 +0100

    perf(apps/api): reduce request throttle limit from 1000 to 600

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -19,7 +19,7 @@ import { SharedModule } from './shared/shared.module';
     ThrottlerModule.forRoot([
       {
         ttl: 60000,
-        limit: 1000,
+        limit: 600,
         skipIf: () => process.env.THROTTLE_ENABLED === 'false',
       },
     ]),
