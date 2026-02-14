99b3cc2b66cf01632a8edbb24419d4afa338bd09
Nikodem Okroj <n.okroj@icloud.com>
2026-02-14 09:27:18 +0100

    refactor(packages/contracts): reorder export statements for library modules

--- packages/contracts/src/index.ts
+++ packages/contracts/src/index.ts
@@ -1,9 +1,9 @@
 export * from "./schemas";
 
-export * from "./library-albums";
 export * from "./auth";
 export * from "./library";
 export * from "./library-artists";
+export * from "./library-albums";
 
 export * from "./api";
 export * from "./utils";
