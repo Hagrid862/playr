1643faa3f0cac44b08ec22e5bd15d9ec561209f7
ressca <112687176+ressca@users.noreply.github.com>
2026-03-13 19:55:31 +0100

    feat(packages/contracts): update login and register zod response schemas

--- packages/contracts/src/auth/response/login.response.ts
+++ packages/contracts/src/auth/response/login.response.ts
@@ -2,11 +2,24 @@ import { z } from "zod";
 import { createApiResponseSchema } from "../../api/response.schema";
 import { UserSchema } from "../../schemas";
 
-export const LoginResponseSchema = createApiResponseSchema(
-  z.object({
+
+export const AuthenticatedLoginResponseSchema = z.object({
+    outcome: z.literal("authenticated"),
     accessToken: z.string(),
     user: UserSchema,
-  }),
+});
+
+export const UnauthenticatedEmailLoginResponseSchema = z.object({
+    outcome: z.literal("unauthenticated"),
+    emailVerificationToken: z.string(),
+    user: UserSchema,
+});
+
+export const LoginResponseSchema = createApiResponseSchema(
+  z.discriminatedUnion("outcome", [
+      AuthenticatedLoginResponseSchema,
+      UnauthenticatedEmailLoginResponseSchema,
+      ]),
 );
 
 export type LoginResponse = z.infer<typeof LoginResponseSchema>;
--- packages/contracts/src/auth/response/register.response.ts
+++ packages/contracts/src/auth/response/register.response.ts
@@ -2,6 +2,11 @@ import { createApiResponseSchema } from "../../api/response.schema";
 import { UserSchema } from "../../schemas/user.schema";
 import { z } from "zod";
 
-export const RegisterResponseSchema = createApiResponseSchema(UserSchema);
+export const RegisterResponseSchema = createApiResponseSchema(
+    z.object({
+        emailVerificationToken: z.string(),
+        user: UserSchema,
+    })
+);
 
 export type RegisterResponse = z.infer<typeof RegisterResponseSchema>;
