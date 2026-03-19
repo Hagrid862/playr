821a65c784403926506ab812da2825095c68ea00
Nikodem Okroj <n.okroj@icloud.com>
2026-03-19 15:26:20 +0100

    test(packages/testing): add default values to request builders

--- packages/testing/src/requests/create-library-album-request.builder.ts
+++ packages/testing/src/requests/create-library-album-request.builder.ts
@@ -1,13 +1,13 @@
+import { randUuid } from "@ngneat/falso";
 import type { CreateLibraryAlbumRequest } from "@repo/contracts";
 import { AlbumType } from "@repo/db";
-import { randUuid } from "@ngneat/falso";
 
 export function createLibraryAlbumRequestBuilder(
   overrides?: Partial<CreateLibraryAlbumRequest>,
 ): CreateLibraryAlbumRequest {
   return {
-    name: "",
-    description: "",
+    name: "Test Album",
+    description: "Test album description",
     type: AlbumType.album,
     artistId: randUuid(),
     releaseDate: null,
