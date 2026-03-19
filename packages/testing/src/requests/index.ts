949ffc38c30b62c3b4aea74a43c506e75e7af66f
Nikodem Okroj <n.okroj@icloud.com>
2026-03-19 12:19:24 +0100

    feat(packages/testing): add requests module export and configuration
    Add new requests module to testing package with request builders for library operations, authentication, and bulk uploads. This provides a centralized export for all request builders used in testing scenarios.
    
    - Add new entry point for requests module in package.json exports
    - Create index file exporting all request builders
    - Update tsup configuration to include requests module in build output
    - Add @repo/contracts dependency for request builder types

---
+++ packages/testing/src/requests/index.ts
@@ -0,0 +1,18 @@
+export { createLibraryAlbumRequestBuilder } from "./create-library-album-request.builder";
+export { createLibraryTrackRequestBuilder } from "./create-library-track-request.builder";
+export { createLibraryArtistRequestBuilder } from "./create-library-artist-request.builder";
+export {
+  bulkCreateLibraryTrackItemBuilder,
+  bulkCreateLibraryTracksRequestBuilder,
+} from "./bulk-create-library-tracks-request.builder";
+export { updateLibraryAlbumRequestBuilder } from "./update-library-album-request.builder";
+export { updateLibraryTrackRequestBuilder } from "./update-library-track-request.builder";
+export { updateLibraryArtistRequestBuilder } from "./update-library-artist-request.builder";
+export { loginRequestBuilder } from "./login-request.builder";
+export { registerRequestBuilder } from "./register-request.builder";
+export { deleteLibraryAlbumRequestBuilder } from "./delete-library-album-request.builder";
+export { deleteLibraryArtistRequestBuilder } from "./delete-library-artist-request.builder";
+export { getLibraryAlbumsRequestBuilder } from "./get-library-albums-request.builder";
+export { getLibraryTracksRequestBuilder } from "./get-library-tracks-request.builder";
+export { getLibraryArtistsRequestBuilder } from "./get-library-artists-request.builder";
+export { bulkUploadTrackAudioRequestBuilder } from "./bulk-upload-track-audio-request.builder";
