9fa9f9b43809c1290fe38bdf28254796bd92fd89
ressca <112687176+ressca@users.noreply.github.com>
2026-03-23 11:58:23 +0100

    feat(apps/api): add logger in otp-code.service

--- apps/api/src/features/auth/services/otp-code.service.ts
+++ apps/api/src/features/auth/services/otp-code.service.ts
@@ -1,10 +1,12 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, Logger, InternalServerErrorException } from '@nestjs/common';
 import { HashingService } from '@/shared/services/hashing.service';
 import { Redis } from 'ioredis';
 import * as crypto from 'crypto';
 
 @Injectable()
 export class OtpCodeService {
+  private readonly logger = new Logger(OtpCodeService.name);
+
   constructor(
     private readonly hashingService: HashingService,
     @Inject('REDIS_CLIENT') private readonly redis: Redis,
@@ -14,14 +16,24 @@ export class OtpCodeService {
     email: string,
     otpType: 'emailVerification' | 'passwordReset',
   ): Promise<string> {
-    const otp = crypto.randomInt(10000000, 99999999).toString();
-    const hashedOtp = await this.hashingService.hash(otp);
-    const expirationTime = 15; // 15 minutes
-    const key = `otp:${otpType}:${email}`;
+    try {
+      const otp = crypto.randomInt(10000000, 99999999).toString();
+      const hashedOtp = await this.hashingService.hash(otp);
+      const expirationTime = 15; // 15 minutes
+      const key = `otp:${otpType}:${email}`;
 
-    await this.redis.set(key, hashedOtp, 'EX', expirationTime * 60);
+      this.logger.log(`Generated OTP for ${email} and type ${otpType}`);
 
-    return otp;
+      await this.redis.set(key, hashedOtp, 'EX', expirationTime * 60);
+
+      return otp;
+
+    } catch (error) {
+      const errorMessage = error instanceof Error ? error.message : 'Unknown error';
+
+      this.logger.error(`Error generating OTP for ${email} and type ${otpType}: ${errorMessage}`);
+      throw new InternalServerErrorException('Failed to generate OTP code');
+    }
   }
 
   async verifyOTPCode(
@@ -31,19 +43,30 @@ export class OtpCodeService {
   ): Promise<boolean> {
     const key = `otp:${otpType}:${email}`;
 
-    const storedOtp = await this.redis.get(key);
+    try {
+      const storedOtp = await this.redis.get(key);
 
-    if (!storedOtp) {
-      return false;
+      if (!storedOtp) {
+        this.logger.warn(`No OTP found for ${email} and type ${otpType}`);
+        return false;
+      }
+
+      const isMatch = await this.hashingService.compare(otp, storedOtp);
+
+      if (isMatch) {
+        this.logger.log(`OTP for ${email} and type ${otpType} verified successfully`);
+        await this.redis.del(key);
+      } else {
+        this.logger.warn(`OTP for ${email} and type ${otpType} verification failed`);
+      }
+
+      return isMatch;
+    } catch (error) {
+      const errorMessage = error instanceof Error ? error.message : 'Unknown error';
+
+      this.logger.error(`Error verifying OTP for ${email} and type ${otpType}: ${errorMessage}`);
+      throw new InternalServerErrorException('Failed to verify OTP code');
     }
-
-    const isMatch = await this.hashingService.compare(otp, storedOtp);
-
-    if (isMatch) {
-      await this.redis.del(key);
-    }
-
-    return isMatch;
   }
 
   async checkIfOTPCodeExist(
@@ -52,8 +75,15 @@ export class OtpCodeService {
   ): Promise<boolean> {
     const key = `otp:${otpType}:${email}`;
 
-    const storedOtp = await this.redis.get(key);
+    try{
+      const storedOtp = await this.redis.get(key);
 
-    return !!storedOtp;
+      return !!storedOtp;
+    } catch (error) {
+      const errorMessage = error instanceof Error ? error.message : 'Unknown error';
+
+      this.logger.error(`Error checking OTP existence for ${email} and type ${otpType}: ${errorMessage}`);
+      throw new InternalServerErrorException('Failed to check OTP code existence');
+    }
   }
 }
