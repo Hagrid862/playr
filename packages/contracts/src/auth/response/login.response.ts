1a06d5a2b49ec39ae7d7932e0b275e17ad5692ba
ressca <112687176+ressca@users.noreply.github.com>
2026-03-24 10:06:09 +0100

    refactor(packages/contracts): add is email sent field to handle edge case where user is registered but email can not be sent

--- packages/contracts/src/auth/response/login.response.ts
+++ packages/contracts/src/auth/response/login.response.ts
@@ -1,24 +1,25 @@
-import { z } from "zod";
+import { boolean, z } from "zod";
 import { createApiResponseSchema } from "../../api/response.schema";
 import { UserSchema } from "../../schemas";
 
 
 export const AuthenticatedLoginResponseSchema = z.object({
-    outcome: z.literal("authenticated"),
-    accessToken: z.string(),
-    user: UserSchema,
+  outcome: z.literal("authenticated"),
+  accessToken: z.string(),
+  user: UserSchema,
 });
 
 export const UnauthenticatedEmailLoginResponseSchema = z.object({
-    outcome: z.literal("unauthenticated"),
-    user: UserSchema,
+  outcome: z.literal("unauthenticated"),
+  user: UserSchema,
+  isEmailSent: boolean,
 });
 
 export const LoginResponseSchema = createApiResponseSchema(
   z.discriminatedUnion("outcome", [
-      AuthenticatedLoginResponseSchema,
-      UnauthenticatedEmailLoginResponseSchema,
-      ]),
+    AuthenticatedLoginResponseSchema,
+    UnauthenticatedEmailLoginResponseSchema,
+  ]),
 );
 
 export type LoginResponse = z.infer<typeof LoginResponseSchema>;
--- packages/contracts/src/auth/response/register.response.ts
+++ packages/contracts/src/auth/response/register.response.ts
@@ -2,6 +2,11 @@ import { createApiResponseSchema } from "../../api/response.schema";
 import { UserSchema } from "../../schemas/user.schema";
 import { z } from "zod";
 
-export const RegisterResponseSchema = createApiResponseSchema( UserSchema );
+export const RegisterResponseSchema = createApiResponseSchema(
+  z.object({
+    user: UserSchema,
+    isEmailSent: boolean,
+  }),
+);
 
 export type RegisterResponse = z.infer<typeof RegisterResponseSchema>;
