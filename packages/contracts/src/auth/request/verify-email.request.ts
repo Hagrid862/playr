e539f9dc5305a976e8b61a79280ef55ae2410799
ressca <112687176+ressca@users.noreply.github.com>
2026-03-25 11:02:10 +0100

    fix(packages/contracts): fix verify email request zod schema to be more descriptive

--- packages/contracts/src/auth/request/verify-email.request.ts
+++ packages/contracts/src/auth/request/verify-email.request.ts
@@ -1,8 +1,14 @@
 import z from "zod";
 
 export const VerifyEmailRequestSchema = z.object({
-  email: z.string().email(),
-  otpCode: z.string().length(8),
+  email: z
+    .email("Invalid email address")
+    .max(256, "Email must be at most 256 characters")
+    .transform((val) => val.toLowerCase().trim()),
+  otpCode: z
+    .string()
+    .length(8)
+    .transform((val) => val.toLowerCase().trim()),
 });
 
 export type VerifyEmailRequest = z.infer<typeof VerifyEmailRequestSchema>;
