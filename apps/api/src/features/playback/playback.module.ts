5b99277beabec35c1c88d0281a28be6245b856de
Nikodem Okroj <n.okroj@icloud.com>
2026-03-25 11:39:05 +0100

    feat(apps/api): add Redis provider for playback gateway

--- apps/api/src/features/playback/playback.module.ts
+++ apps/api/src/features/playback/playback.module.ts
@@ -1,11 +1,28 @@
+import { Env } from '@/common/config/env.schema';
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Redis from 'ioredis';
 import { AuthModule } from '../auth/auth.module';
 import { PlaybackGateway } from './playback.gateway';
+
+export const PLAYBACK_REDIS = Symbol('PLAYBACK_REDIS');
 
 @Module({
   imports: [AuthModule],
   controllers: [],
-  providers: [PlaybackGateway],
-  exports: [],
+  providers: [
+    PlaybackGateway,
+    {
+      provide: PLAYBACK_REDIS,
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService<Env>) =>
+        new Redis({
+          host: configService.getOrThrow('REDIS_HOST'),
+          port: configService.getOrThrow('REDIS_PORT'),
+          keyPrefix: 'playr:playback:',
+        }),
+    },
+  ],
+  exports: [PLAYBACK_REDIS],
 })
 export class PlaybackModule {}
