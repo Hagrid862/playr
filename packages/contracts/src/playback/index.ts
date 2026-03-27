941c08ad823b7e29b29c2d90ac731c64ba562dd9
Nikodem Okroj <n.okroj@icloud.com>
2026-03-27 15:34:18 +0100

    feat(packages/contracts): add granular playback state request schemas
    Add separate request schemas for each playback property (playing,
    repeat, shuffle, track, volume, current time). These enable granular
    state updates with optimistic locking support via expectedVersion.
    Also fix typo in playback state request export path.

--- packages/contracts/src/playback/index.ts
+++ packages/contracts/src/playback/index.ts
@@ -1,7 +1,13 @@
 // Requests
 export * from "./request/get-playback-state.request";
-export * from "./request/set-playback-state.reqeust";
-
+export * from "./request/set-current-time-state.request";
+export * from "./request/set-playback-state.request";
+export * from "./request/set-playing-state.request";
+export * from "./request/set-repeat-state.request";
+export * from "./request/set-shuffle-state.request";
+export * from "./request/set-track-state.request";
+export * from "./request/set-volume-level-state.request";
 // Responses
 export * from "./response/get-playback-state.response";
 export * from "./response/set-playback-state.response";
+
---
+++ packages/contracts/src/playback/request/set-playing-state.request.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const SetPlayingStateRequestSchema = z
+  .object({
+    isPlaying: z.boolean(),
+    expectedVersion: z.number().int().min(0).default(0),
+  })
+  .strict();
+
+export type SetPlayingStateRequest = z.infer<
+  typeof SetPlayingStateRequestSchema
+>;
