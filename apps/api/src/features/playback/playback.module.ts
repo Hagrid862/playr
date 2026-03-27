b5e6ad52c2b9fd3c2bf6934367365b034c112d92
Nikodem Okroj <n.okroj@icloud.com>
2026-03-27 17:06:21 +0100

    feat(apps/api): register playback state command handlers

--- apps/api/src/features/playback/playback.module.ts
+++ apps/api/src/features/playback/playback.module.ts
@@ -4,14 +4,32 @@ import { ConfigService } from '@nestjs/config';
 import { CqrsModule } from '@nestjs/cqrs';
 import Redis from 'ioredis';
 import { AuthModule } from '../auth/auth.module';
+import { SetCurrentTimeStateHandler } from './commands/handlers/set-current-time-state.handler';
+import { SetFavoriteStateHandler } from './commands/handlers/set-favorite-state.handler';
+import { SetLibraryStateHandler } from './commands/handlers/set-library-state.handler';
 import { SetPlaybackStateHandler } from './commands/handlers/set-playback-state.handler';
+import { SetPlayingStateHandler } from './commands/handlers/set-playing-state.handler';
+import { SetRepeatStateHandler } from './commands/handlers/set-repeat-state.handler';
+import { SetShuffleStateHandler } from './commands/handlers/set-shuffle-state.handler';
+import { SetTrackStateHandler } from './commands/handlers/set-track-state.handler';
+import { SetVolumeLevelStateHandler } from './commands/handlers/set-volume-level.handler';
 import { PlaybackGateway } from './playback.gateway';
 import { GetPlaybackStateHandler } from './queries/handlers/get-playback-state.handler';
 import { PlaybackStatePersistenceService } from './services/playback-state-persistence.service';
 import { PLAYBACK_REDIS } from './utils/playback-redis.constrants';
 
 export const QueryHandlers = [GetPlaybackStateHandler];
-export const CommandHandlers = [SetPlaybackStateHandler];
+export const CommandHandlers = [
+  SetPlaybackStateHandler,
+  SetPlayingStateHandler,
+  SetShuffleStateHandler,
+  SetRepeatStateHandler,
+  SetTrackStateHandler,
+  SetVolumeLevelStateHandler,
+  SetLibraryStateHandler,
+  SetFavoriteStateHandler,
+  SetCurrentTimeStateHandler,
+];
 
 @Module({
   imports: [AuthModule, CqrsModule],
