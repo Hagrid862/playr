02463e563b74ec57929eb1a09a913e2f27dca56e
ressca <112687176+ressca@users.noreply.github.com>
2026-04-02 23:15:47 +0200

    feat(apps/web): add useVerifyEmail hook for with tanstack query for sending api request

---
+++ apps/web/src/hooks/api/auth/requests/verifyEmail.ts
@@ -0,0 +1,10 @@
+import { VerifyEmailRequest, VerifyEmailResponse, VerifyEmailResponseSchema } from "@repo/contracts";
+import { apiClient } from "@/lib/api-client";
+
+export const verifyEmail = (data: VerifyEmailRequest) => {
+	return apiClient<VerifyEmailResponse>('auth/verify-email', {
+		method: 'POST',
+		body: data,
+		zodSchema: VerifyEmailResponseSchema,
+	});
+};
