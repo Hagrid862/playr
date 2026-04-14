12d5321d73357e6afd203b8ef58f00bde4c5561a
ressca <112687176+ressca@users.noreply.github.com>
2026-04-14 13:51:36 +0200

    feat(apps/web): complete verify email route currently with temporary placeholder form

--- apps/web/src/routes/auth/verify-email.tsx
+++ apps/web/src/routes/auth/verify-email.tsx
@@ -1,8 +1,9 @@
 import { createFileRoute, redirect, useNavigate } from '@tanstack/react-router';
 import { z } from 'zod';
-import { SyntheticEvent, useMemo } from 'react';
-import { useResendEmailVerificationCode } from '@/hooks/api/auth';
+import { SyntheticEvent, useEffect, useMemo } from 'react';
+import { useResendEmailVerificationCode, useVerifyEmail } from '@/hooks/api/auth';
 import { useAuthStore } from '@/stores/auth.store.ts';
+import { useVerifyEmailForm } from '@/hooks/forms/useVerifyEmailForm.ts';
 
 const verifyEmailSearchSchema = z.object({
   isVerificationEmailSent: z
@@ -26,6 +27,13 @@ export const Route = createFileRoute('/auth/verify-email')({
     if (isAuthenticated) {
       throw redirect({ to: '/app' });
     }
+
+    if (_hasHydrated) {
+      const hasPrimaryEmail = user?.emailAddresses?.some((e) => e.type === 'primary');
+      if (!hasPrimaryEmail) {
+        console.error('internal application error: user has no primary email address');
+      }
+    }
   },
 });
 
@@ -36,6 +44,13 @@ export function RouteComponent() {
     mutateAsync: resendEmailVerificationCode,
     isPending: resendEmailVerificationCodeIsLoading,
   } = useResendEmailVerificationCode();
+  const {
+    mutateAsync: verifyEmail,
+  } = useVerifyEmail();
+  const {
+    handleChange,
+    handleSubmit,
+  } = useVerifyEmailForm();
 
   const user = useAuthStore((state) => state.user);
 
@@ -43,17 +58,31 @@ export function RouteComponent() {
     return user?.emailAddresses?.find((e) => e.type === 'primary')?.email;
   }, [user]);
 
+  //puts email into the form data when the store hydrates, so the user doesn't have to pointlessly enter email
+  useEffect(() => {
+    if(primaryEmail) {
+      handleChange('email', primaryEmail);
+    }
+  }, [primaryEmail, handleChange]);
+
   const onSubmit = async (e: SyntheticEvent<HTMLFormElement>) => {
     e.preventDefault();
+    const data = handleSubmit();
+    if (!data) return;
 
-    if (!primaryEmail) {
-      console.error('No primary email found for the user while trying to verify email.');
-      return;
+    try {
+      const result = await verifyEmail(data);
+      if (result.success){
+        useAuthStore.getState().setAuth(result.data.user, result.data.accessToken);
+
+        await navigate({ to: '/' });
+
+      } else {
+        console.error('Failed to verify email');
+      }
+    } catch (err) {
+      console.error('Failed to verify email', err);
     }
-
-    // TODO add here verify email from useVerifyEmail after useVerifyEmailForm is done
-
-    await navigate({ to: '/' });
   };
 
   const onResendEmail = async () => {
@@ -64,7 +93,11 @@ export function RouteComponent() {
       return;
     }
 
-    await resendEmailVerificationCode({ email: primaryEmail });
+    try{
+      await resendEmailVerificationCode({ email: primaryEmail });
+    } catch (err) {
+      console.error('Failed to resend email verification code', err);
+    }
   };
 
   // Placeholder component only to test logic and flow will be replaced with actual UI later.
@@ -73,6 +106,7 @@ export function RouteComponent() {
       {/* TODO make an actual page and a component and put it here.*/}
       <p>Send verification email: {isVerificationEmailSent ? 'Yes' : 'No'}</p>
       <form onSubmit={onSubmit}>
+        <input type="text" onChange={(e) => handleChange('otpCode', e.target.value)} placeholder="Enter OTP" />
         <input type="submit" value="verify email" />
       </form>
 
