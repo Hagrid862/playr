d981af41dc38d449d95742d7f30e93e7d5544880
Nikodem Okroj <n.okroj@icloud.com>
2026-04-27 12:00:47 +0200

    refactor(apps/api): remove deletedAt filter from album query to streamline data retrieval

--- apps/api/src/features/library-albums/queries/handlers/get-library-album.handler.ts
+++ apps/api/src/features/library-albums/queries/handlers/get-library-album.handler.ts
@@ -25,7 +25,6 @@ export class GetLibraryAlbumHandler implements IQueryHandler<GetLibraryAlbumQuer
             artists: true,
             album: {
               include: { cover: true },
-              where: { deletedAt: null },
             },
           },
         },
