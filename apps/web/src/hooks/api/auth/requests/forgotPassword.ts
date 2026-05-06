5b5f42ecbed087e5ad77a6933bfbae47ef469aae
ressca <112687176+ressca@users.noreply.github.com>
2026-05-06 18:15:32 +0200

    feat(apps/api): add password reset route

--- apps/web/src/hooks/api/auth/requests/forgotPassword.ts
+++ apps/web/src/hooks/api/auth/requests/forgotPassword.ts
@@ -3,7 +3,7 @@ import {apiClient} from "@/lib/api-client.ts";
 import { ForgotPasswordResponseSchema } from '@repo/contracts';
 
 export const forgotPassword = (data: ForgotPasswordRequest) => {
-	return apiClient<ForgotPasswordResponse>('auth/forget-password', {
+	return apiClient<ForgotPasswordResponse>('auth/forgot-password', {
 		method: 'POST',
 		body: data,
 		zodSchema: ForgotPasswordResponseSchema
