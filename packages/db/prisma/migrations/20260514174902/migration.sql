cf8504c7a1c07befe48b8cd866e63af36cc7adfa
ressca <112687176+ressca@users.noreply.github.com>
2026-05-15 21:03:44 +0200

    fix(packages/db): fix issues reported by code rabbit

--- packages/db/prisma/migrations/20260514174902/migration.sql
+++ packages/db/prisma/migrations/20260514174902/migration.sql
@@ -1,11 +1,2 @@
 -- CreateEnum
 CREATE TYPE "OtpCodeType" AS ENUM ('emailVerification', 'passwordReset');
-
--- DropIndex
-DROP INDEX "albums_name_trgm_idx";
-
--- DropIndex
-DROP INDEX "artists_name_trgm_idx";
-
--- DropIndex
-DROP INDEX "tracks_title_trgm_idx";
